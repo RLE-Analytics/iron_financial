bbffc01b22e14ef9c8355c0bb5748fbfc6e170a7
rozran00@gmail.com <rozran00@gmail.com>
2023-03-26 16:35:16 -0400

    reversing order

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -175,8 +175,8 @@ def get_simulation(symbol,
             sum(final_prices > strp) / num_samples)
     
     calls = opt_chain.loc[opt_chain['option_type'] == 'call']
-    calls['strike_plus_ask'] = calls['strike'] - calls['ask']
-    calls['strike_minus_bid'] = calls['strike'] + calls['bid']
+    calls['strike_plus_ask'] = calls['strike'] + calls['ask']
+    calls['strike_minus_bid'] = calls['strike'] - calls['bid']
 
     for strp in calls.strike:
         calls.loc[calls['strike'] == strp, 'likelihood_above'] = (
