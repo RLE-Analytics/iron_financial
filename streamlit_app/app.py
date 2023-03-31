ef0c6a2e0b14e2b7913d18c76e04aa3d841b94ce
rozran00@gmail.com <rozran00@gmail.com>
2023-03-31 06:06:23 -0400

    gutted

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -229,105 +229,6 @@ def get_simulation(symbol,
     
     return(puts, calls, final_prices, hist_price)
 
-def price_chart(hist_price, STOCK):
-    cutoff = datetime.today().date() - timedelta(days = 365)
-    cutoff = cutoff.isoformat()
-    
-    hist_price = hist_price.loc[hist_price['Date'] > cutoff]
-    
-    fig = px.line(hist_price, 
-                  x = 'Date', 
-                  y = 'Close',
-                  title = f'{STOCK} Prices for Last Year')
-    fig.update_layout(yaxis_tickprefix = '$')
-    return(fig)
-
-def box_final_prices(final_prices, current_price):
-    fig = px.box(final_prices,
-                 x = "final_price",
-                 labels = {"final_price": "Final Simulated Price"},
-                 title = "Boxplot of Simulated Prices (Current stock price in red)")
-    fig.add_vline(x = current_price, line_color = 'firebrick')
-    fig.update_layout(xaxis_tickprefix = '$')
-    return(fig)
-
-def hist_final_prices(final_prices, current_price):
-    fig = px.histogram(final_prices, 
-                       x = "final_price", 
-                       histnorm = 'probability density',
-                       labels = {"final_price": "Final Simulated Price"},
-                       title = "Histogram of Simulated Prices (Current stock price in red)")
-    fig.add_vline(x = current_price, line_color = 'firebrick')
-    fig.update_layout(xaxis_tickprefix = '$')
-    return(fig)
-
-def get_latest_day(stock, current_date = datetime.now()):
-    if ('-USD' in stock or 
-            (current_date.date().weekday() >= 1 and 
-                current_date.date().weekday() <= 4)):
-        current_date = current_date.date()
-    else:
-        if current_date.date().weekday() == 5:
-            current_date = current_date.date() - timedelta(days = 1)
-        elif current_date.date().weekday() == 6: 
-            current_date = current_date.date() - timedelta(days = 2)
-        else: 
-            if current_date.hour + (current_date.minute / 60) < 9.5:
-                current_date = current_date.date() - timedelta(days = 3)
-            else:
-                current_date = current_date.date()
-    
-    return(current_date)
-
-def strike_to_effective_plot(dat, current_price, puts = True):
-    if puts:
-        dat['ask'] = dat['ask'] * -1
-    
-    dat['Strike Price'] = dat['strike']
-    
-    fig = px.line(dat, x = 'Strike Price', y = 'Effective Price')
-    fig = fig.add_bar(x = dat['Strike Price'],
-                      y = dat['Strike Price'])
-    fig = fig.add_bar(x = dat['Strike Price'],
-                      y = dat['ask'])
-    fig.add_hline(y = current_price, line_color = 'firebrick')
-    fig.update_layout(xaxis_tickprefix = '$',
-                      yaxis_tickprefix = '$',
-                      showlegend = False)
-    return(fig)
-    
-
-def effective_to_prob(dat, current_price, puts):
-    if puts:
-        dat['Llhd Blw EP'] = dat['Llhd Blw EP'] * 100
-        fig = px.bar(dat, x = 'strike', y = 'Llhd Blw EP')
-    else:
-        dat['Llhd Abv EP'] = dat['Llhd Abv EP'] * 100
-        fig = px.bar(dat, x = 'strike', y = 'Llhd Abv EP')
-    fig.add_vline(x = current_price, line_color = 'firebrick')
-    fig.update_layout(xaxis_tickprefix = '$',
-                      yaxis_ticksuffix = '%')
-    return(fig)
-
-def strike_to_prob(dat, current_price, puts):
-    if puts:
-        plot_dat = dat[['strike', 'Llhd Blw Stk']]
-        plot_dat['Llhd Blw Stk'] = plot_dat['Llhd Blw Stk'] * 100
-        fig = px.bar(plot_dat, x = 'strike', y = 'Llhd Blw Stk')
-    else:
-        plot_dat = dat[['strike', 'Llhd Abv Stk']]
-        plot_dat['Llhd Abv Stk'] = plot_dat['Llhd Abv Stk'] * 100
-        fig = px.bar(plot_dat, x = 'strike', y = 'Llhd Abv Stk')
-    fig.add_vline(x = current_price, line_color = 'firebrick')
-    fig.update_layout(xaxis_tickprefix = '$',
-                      yaxis_ticksuffix = '%')
-    return(fig)
-
-def get_strike_prices(dat):
-    strikes = dat['strike'].tolist()
-    
-    return(strikes)
-
 def get_string_info(dat, strike):
     
     bid = dat.loc[dat['strike'] == strike, 'bid'].values[0]
@@ -348,7 +249,7 @@ def main() -> None:
     
     st.sidebar.subheader('Evaluate What Stock Options?')
     
-    STOCK = st.sidebar.text_input('Stock:', 'AAPL')
+    STOCK = 'AAPL'
     
     st.sidebar.subheader('What Option Expiration Date?')
     
@@ -361,153 +262,36 @@ def main() -> None:
     
     price = get_current_price(STOCK, token)
     
-    st.header(f'Options Evaluations for {STOCK}')
+    st.header(f'Buying Put Options for 10 Largest Market Cap Stocks')
     st.text(f'Option Expiration Date of {options_selection}')
     st.text(f'Current stock price: ${price}')
     
     puts, calls, final_prices, hist_price = get_simulation(STOCK, 
                                                            token,
                                                            options_selection)
-                                                           
-    strikes = get_strike_prices(puts)
-    strike_selection = st.sidebar.selectbox(
-        "Select Strike Price:", 
-        options = strikes
-    )
+                                                      
     
-    put_tab, call_tab, price_tab, hist_tab = st.tabs([f'{STOCK} Puts', 
-                                                      f'{STOCK} Calls', 
-                                                      f'Simulated {STOCK} Prices',
-                                                      f'Historical {STOCK} Prices'])
+    puts.fillna(0, inplace = True)
+            
+    puts['Cost to Buy Option'] = puts['ask'] * -100
+    puts['Potential Gain'] = (puts['strike'] - puts['EV Blw EP'] - puts['ask']) * 100
+    puts['Llhd Abv EP'] = 1 - puts['Llhd Blw EP']
     
-    with put_tab:
-        st.subheader("Put Options Data")
-        
-        bid, ask, bid100, ask100, ep_buy, ep_sell, strike100 = get_string_info(puts, strike_selection)
-        
-        sell_tab, buy_tab = st.tabs([f'Sell {STOCK} Puts',
-                                     f'Buy {STOCK} Puts'])
-        
-        with sell_tab:
-            
-            puts.fillna(0, inplace = True)
-            
-            sts = (f'If I sell a put for a strike of ${strike_selection} (expires on {options_selection}) for ${bid}:\n\t- I make ${bid100} if the stock stays above ${ep_sell} (I want that to happen)\n\t- I have to buy the stock if it goes below ${ep_sell} (I don\'t want that to happen)\n\t- I need to have ${strike100} (${strike_selection} * 100) collateral')
-            
-            st.text(sts)
-            
-            puts_sell = puts[['strike',
-                          'bid',
-                          'Effective Price (sell)',
-                          'Llhd Abv EP',
-                          'Llhd Abv Stk',
-                          'EV Abv EP']]
-        
-            st.dataframe(puts_sell)
-            
-            
-        
-        with buy_tab:
-            
-            puts.fillna(0, inplace = True)
-            
-            puts['Cost to Buy Option'] = puts['ask'] * -100
-            puts['Potential Gain'] = (puts['strike'] - puts['EV Blw EP'] - puts['ask']) * 100
-            puts['Llhd Abv EP'] = 1 - puts['Llhd Blw EP']
-            
-            puts['Expected Value'] = ((puts['Llhd Abv EP'] * puts['Cost to Buy Option']) + 
-                                      (puts['Llhd Blw EP'] * puts['Potential Gain']))
-            
-            puts_buy = puts[['strike',
-                             'ask',
-                             'Effective Price (buy)',
-                             'Llhd Blw EP',
-                             'Llhd Abv EP',
-                             'EV Blw EP',
-                             'Cost to Buy Option',
-                             'Potential Gain',
-                             'Expected Value']]
-            
-            payout = puts_buy.loc[puts_buy['strike'] == strike_selection, 'Expected Value'].values[0]
-            
-            sts = (f'If I buy a put for a strike of ${strike_selection} (expires on {options_selection}) for ${ask}:\n\t- I lose ${ask100} if the stock stays above ${ep_buy} (I don\'t want that to happen)\n\t- I can buy the stock if it goes below ${ep_buy} (I want that to happen)\n\t- I will make money if the stock goes below ${ep_buy}\n\t- If I make this play, I can expect a payout of {payout}')
-            
-            st.text(sts)
-        
-            st.dataframe(puts_buy)
-            
-            
-            # bar = strike_to_effective_plot(puts, price, True)
-            # st.plotly_chart(bar)
-            # 
-            # prob_bar = effective_to_prob(puts, price, True)
-            # st.plotly_chart(prob_bar)
-            # 
-            # strike_bar = strike_to_prob(puts, price, True)
-            # st.plotly_chart(strike_bar)
+    puts['Expected Value'] = ((puts['Llhd Abv EP'] * puts['Cost to Buy Option']) + 
+                              (puts['Llhd Blw EP'] * puts['Potential Gain']))
     
-    with call_tab:
-        st.subheader("Call Options Data")
-        
-        bid, ask, bid100, ask100, ep_buy, ep_sell, strike100 = get_string_info(calls, strike_selection)
-        
-        sell_tab, buy_tab = st.tabs([f'Sell {STOCK} Calls',
-                                     f'Buy {STOCK} Calls'])
-        
-        with sell_tab:
-        
-            sts = (f'If I sell a call for a strike of ${strike_selection} (expires on {options_selection}) for ${bid}:\n\t- I make ${bid100} if the stock stays below ${ep_sell} (I want that to happen)\n\t- I have to sell the stock if it goes above ${ep_sell} (I don\'t want that to happen)\n\t- I need to have 100 shares collateral')
-            
-            st.text(sts)
-            
-            calls_sell = calls[['strike',
-                                'bid',
-                                'Effective Price (sell)',
-                                'Llhd Blw EP',
-                                'Llhd Blw Stk',
-                                'EV Blw EP']]
-        
-            st.dataframe(calls_sell)
-            
-            
-        
-        with buy_tab:
-        
-            sts = (f'If I buy a call for a strike of ${strike_selection} (expires on {options_selection}) for ${ask}:\n\t- I lose ${ask100} if the stock stays below ${ep_buy} (I don\'t want that to happen)\n\t- I can buy the stock if it goes above ${ep_buy} (I want that to happen)\n\t- I will make money if the stock goes above ${ep_buy}\n\t- Things to figure out: what is the expected value if it does go below AND what is the overall expected payout.')
-            
-            st.text(sts)
-            
-            calls_buy = calls[['strike',
-                               'ask',
-                               'Effective Price (buy)',
-                               'Llhd Abv EP',
-                               'Llhd Abv Stk',
-                               'EV Abv EP']]
-        
-            st.dataframe(calls_buy)
-            
-            
-            # bar = strike_to_effective_plot(puts, price, True)
-            # st.plotly_chart(bar)
-            # 
-            # prob_bar = effective_to_prob(puts, price, True)
-            # st.plotly_chart(prob_bar)
-            # 
-            # strike_bar = strike_to_prob(puts, price, True)
-            # st.plotly_chart(strike_bar)
-        
-    with price_tab:
-        final_prices = pd.DataFrame({"final_price": final_prices})
-        
-        box = box_final_prices(final_prices, price)
-        st.plotly_chart(box, use_container_width = True)
-        
-        hist = hist_final_prices(final_prices, price)
-        st.plotly_chart(hist, use_container_width = True)
+    puts_buy = puts[['strike',
+                     'ask',
+                     'Effective Price (buy)',
+                     'Llhd Blw EP',
+                     'Llhd Abv EP',
+                     'EV Blw EP',
+                     'Cost to Buy Option',
+                     'Potential Gain',
+                     'Expected Value']]
     
-    with hist_tab:
-        price_line = price_chart(hist_price, STOCK)
-        st.plotly_chart(price_line, use_container_width = True)
+    st.dataframe(puts_buy)
+            
     
 if __name__ == "__main__":
     st.set_page_config(
